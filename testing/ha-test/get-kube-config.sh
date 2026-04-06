67751f7f03dbddc9c0fb118f59835a9ca9458edf
Kurian Thomas Pulimoottil <kurian.thomas.pulimoottil@gmail.com>
2026-04-06 00:06:46 -0700

    documented phase 1 node setup with fixes

--- testing/ha-test/get-kube-config.sh
+++ testing/ha-test/get-kube-config.sh
@@ -1,8 +1,8 @@
 # check ansible group vars
 
 mkdir -p ~/.kube
-ssh -i ~/.ssh/k8s_ansible_key ansible@192.168.122.107 "sudo cp /etc/kubernetes/admin.conf /home/ansible/config && sudo chown ansible:ansible /home/ansible/config"
-scp -i ~/.ssh/k8s_ansible_key ansible@192.168.122.107:/home/ansible/config ~/.kube/config
+ssh -i ~/.ssh/k8s_ansible_key ansible@192.168.122.45 "sudo cp /etc/kubernetes/admin.conf /home/ansible/config && sudo chown ansible:ansible /home/ansible/config"
+scp -i ~/.ssh/k8s_ansible_key ansible@192.168.122.45:/home/ansible/config ~/.kube/config
 
 watch -n 2 kubectl get nodes
 
